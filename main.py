2844c647621def10f8f86a2cb2bb864640506430
harshinee challa <97088825+harshinee-challa@users.noreply.github.com>
2024-02-28 16:19:08 +0530

    Update main.py
    job description enhancement

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from azure.storage.blob import BlobServiceClient
+from dotenv import load_dotenv
 from  view import output
 from  controller import ResumeController
 from getfiles import  Database
 from fastapi import FastAPI,Body
 from fastapi.responses import HTMLResponse
+from jd import ChatBotModel
+import os
 
+# Load environment variables from .env file
+load_dotenv()
+api_key = os.getenv("API_KEY")
 app = FastAPI()
 
 account_name="resumeshortlisting"
@@ -16,11 +22,12 @@ container_client = blob_service_client.get_container_client(container_name)
 database=Database(account_name, account_key, container_name, connect_str, container_client)
 view = output()
 controller =ResumeController(view,database)
-
+jd_enhance=ChatBotModel(api_key)
 @app.get("/shortlist")   
 def shortlist(item: dict = Body(...)):
     jd = item['jd']
-    skills = controller.extract_skills_nlp_based(jd)
+    jd_new = jd_enhance.prompt(jd)
+    skills = controller.extract_skills_nlp_based(jd_new)
     match_list = controller.shortlist(skills)
     resume_scores=controller.process_resume(match_list) 
     ans=view.display_final_list(resume_scores)
